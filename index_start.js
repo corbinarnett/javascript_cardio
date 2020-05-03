f39dea853c37b6a2672e691f4dd7faef689fd6c8
corbinarnett <corbin.c.arnett@gmail.com>
2020-05-02 19:29:47 -0700

    first commit

--- index_start.js
+++ index_start.js
@@ -3,18 +3,17 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  return str.split("").reverse().join("");
 }
-
-
 
 // CHALLENGE 2: VALIDATE A PALINDROME
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) {}
-
-
+function isPalindrome(str) {
+  let revString = str.split("").reverse().join("");
+  return revString === str;
+}
 
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
@@ -22,29 +21,31 @@ function isPalindrome(str) {}
 
 function reverseInt(int) {}
 
-
-
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {}
-
-
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
 function maxCharacter(str) {}
 
-
-
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
-
-
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else console.log(i);
+  }
+}
 
 // Call Function
-const output = reverseString('hello');
+const output = fizzBuzz(100);
 
 console.log(output);
